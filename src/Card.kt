c685a5e92564e98451e25085488556fc4714a5d4
Amitav Khandelwal <amitav.khandelwal@gmail.com>
2017-06-01 14:54:54 +0530

    Completed models for Deck, TableauPile, Card, FoundationPile

--- src/Card.kt
+++ src/Card.kt
@@ -1,4 +1,11 @@
 /**
  * Created by amitavk on 29/05/17.
  */
+val clubs = "Clubs"
+val diamonds = "Diamonds"
+val hearts = "Hearts"
+val spades = "Spades"
+val redSuits = arrayOf(diamonds, hearts)
+val blackSuits = arrayOf(clubs, spades)
+
 class Card(val value: Int, val suit: String, var faceUp: Boolean = false)
---
+++ src/GameModel.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by amitavk on 29/05/17.
+ */
+class GameModel {
+    val deck = Deck()
+    val wastePile: MutableList<Card> = mutableListOf()
+    val foundationPiles= arrayOf(FoundationPile(clubs), FoundationPile(diamonds),
+            FoundationPile(hearts), FoundationPile(spades))
+    val tableauPiles = Array(7, { TableauPile()})
+}
